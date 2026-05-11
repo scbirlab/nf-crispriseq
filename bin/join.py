defa12fba2b0dd7352ae00009caf1a399e7fbdf5
Eachan Johnson <eachan.johnson@crick.ac.uk>
2026-05-11 12:43:49 +0100

    Tidy up

--- bin/join.py
+++ bin/join.py
@@ -14,20 +14,32 @@ except IndexError:
 
 right = pd.read_csv(right_input, sep=sep2)
 
-for i, left_chunk in enumerate(pd.read_csv(sys.stdin, sep="\t", chunksize=10000)):
+for i, left_chunk in enumerate(pd.read_csv(
+    sys.stdin, 
+    sep="\t", 
+    chunksize=10000,
+)):
 
     if i == 0:
         common_cols = set(left_chunk.columns).intersection(set(right.columns))
 
         if len(common_cols) == 0:
-            raise AttributeError(f'No shared columns!\nLeft: {", ".join(left_chunk.columns)}\nRight: {", ".join(right.columns)}')
+            raise AttributeError(
+                f'No shared columns!\nLeft: {", ".join(left_chunk.columns)}\nRight: {", ".join(right.columns)}'
+            )
 
-        print("Joining on shared columns {}".format(", ".join(sorted(common_cols))),
-             file=sys.stderr)
+        print(
+            "Joining on shared columns {}".format(", ".join(sorted(common_cols))),
+            file=sys.stderr,
+        )
     
-    (pd.merge(left_chunk, right, how='left')
-     .to_csv(sys.stdout, 
-             mode='w' if i == 0 else 'a',
-             header=i == 0,
-             index=False, 
-             sep='\t'))
+    (
+        pd.merge(left_chunk, right, how='left')
+        .to_csv(
+            sys.stdout, 
+            mode='w' if i == 0 else 'a',
+            header=i == 0,
+            index=False, 
+            sep='\t',
+        )
+    )
